85c385d25b95e9341bcd9482c39f6d815fe08629
SakshamSaxena97 <sakshamsaxena1997@gmail.com>
2017-09-07 18:44:18 +0530

    paas

---
+++ webcontent/scripts/code.java
@@ -0,0 +1,6 @@
+public class code {
+ public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        ystem.out.println("Hello, World");
+    }
+}
---
+++ webcontent/scripts/code.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python2
+
+x=raw_input()
+y=raw_input()
+print x
+print y
